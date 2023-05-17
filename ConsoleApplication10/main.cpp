7d52e8cd3c8f85e6bb8d924160260a6f4098393e
Alex <example@gmail.com>
2023-05-17 20:33:11 +0300

    Add project files.

---
+++ ConsoleApplication10/main.cpp
@@ -0,0 +1,111 @@
+﻿#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
+struct Field {
+    std::string name;
+    uint32_t mask;
+    std::string description;
+};
+
+struct Register {
+    std::string name;
+    uint32_t address;
+    uint32_t RAW_value;
+    std::vector<Field> fields;
+};
+
+static void from_json(const json& j, Field& field) {
+    j.at("name").get_to(field.name);
+    j.at("mask").get_to(field.mask);
+    j.at("description").get_to(field.description);
+}
+
+static void from_json(const json& j, Register& reg) {
+    j.at("name").get_to(reg.name);
+    j.at("address").get_to(reg.address);
+    j.at("RAW_value").get_to(reg.RAW_value);
+    j.at("fields").get_to(reg.fields);
+}
+
+static std::vector<Register> parse_registers(const std::string& file_path) {
+    std::ifstream file(file_path);
+    if (!file.is_open()) {
+        throw std::runtime_error("Could not open file: " + file_path);
+    }
+    json j;
+    file >> j;
+    return j.at("registers").get<std::vector<Register>>();
+}
+
+static void apply_values(std::vector<Register>& registers, const std::string& file_path) {
+    std::ifstream file(file_path);
+    if (!file.is_open()) {
+        throw std::runtime_error("Could not open file: " + file_path);
+    }
+    json j;
+    file >> j;
+    for (auto& reg : registers) {
+        if (j.contains(reg.name)) {
+            reg.RAW_value = j.at(reg.name).get<uint32_t>();
+        }
+    }
+}
+
+static void print_parsed_fields(const std::vector<Register>& registers) {
+    for (const auto& reg : registers) {
+        std::cout << "Register: " << reg.name << " (0x" << std::hex << reg.address << ")\n";
+        std::cout << "RAW Value: 0x" << std::hex << reg.RAW_value << "\n";
+
+        for (const auto& field : reg.fields) {
+            uint32_t field_value = (reg.RAW_value & field.mask);
+            unsigned long shift;
+            #ifdef __GNUC__
+            shift = __builtin_ctz(field.mask);
+            #elif defined(_MSC_VER)
+            _BitScanForward(&shift, field.mask);
+            #else
+            shift = 0;
+            while (((field.mask >> shift) & 1) == 0) {
+                shift++;
+            }
+            #endif
+            field_value >>= shift;
+            std::cout << "  " << field.name << ": 0x" << std::hex << field_value << " (" << field.description << ")\n";
+        }
+
+        std::cout << std::endl;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <registers.json> <values.json>\n";
+        return 1;
+    }
+
+    try {
+        const std::string schema_file_path = argv[1];
+        const std::string values_file_path = argv[2];
+        auto registers = parse_registers(schema_file_path);
+        apply_values(registers, values_file_path);
+        print_parsed_fields(registers);
+    }
+    catch (const json::exception& e) {
+        std::cerr << "Error parsing JSON: " << e.what() << '\n';
+        return 1;
+    }
+    catch (const std::runtime_error& e) {
+        std::cerr << "Error: " << e.what() << '\n';
+        return 1;
+    }
+
+    return 0;
+}
+
+
+
